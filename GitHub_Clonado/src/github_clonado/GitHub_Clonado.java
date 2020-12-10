5bd5c86437cbe22b44f8fe7b8b5f5abe3eb947bd
Toni <Toni@DESKTOP-0U09ECC.home>
2020-12-10 20:40:48 +0100

    Funcion Multiplcacion

--- GitHub_Clonado/src/github_clonado/GitHub_Clonado.java
+++ GitHub_Clonado/src/github_clonado/GitHub_Clonado.java
@@ -5,7 +5,19 @@ public class GitHub_Clonado {
 
 
     public static void main(String[] args) {
+     int metodosuma;
+     int metodomultiplicacion;
      
+      metodomultiplicacion = multiplicacion(8,2);   
+      
+        System.out.println("El resultado de la multiplicacion es: "+metodomultiplicacion);
     }
     
+     public static int multiplicacion (int a, int b){
+        int resultado;
+        resultado = a + b;
+        return resultado;
+    }
+  
 }
+
